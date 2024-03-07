c721755abc5a6de8f1feb8b28c7db6d39eb08b76
mac <mac@macs-MacBook-Pro.local>
2024-03-07 20:11:47 +0100

    update

--- resources/js/pages/managers/dashboard.tsx
+++ resources/js/pages/managers/dashboard.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import { Text } from '@chakra-ui/react';
 import ManagerDashboardLayout from '@/layout/managers/manager-dashboard-layout';
+import useIsPartner from '@/hooks/use-is-partner';
+import useSharedProps from '@/hooks/use-shared-props';
+import route from '@/util/route';
+import Slab, { SlabBody } from '@/components/slab';
 
 interface Props {
   // users: PaginationResponse<User>;
 }
 
 function ManagerDashboard({}: Props) {
+  const isPartner = useIsPartner();
+  const { currentUser } = useSharedProps();
+  const onboardingUrl = route('registration-requests.create', [currentUser]);
   return (
     <ManagerDashboardLayout>
+      {isPartner && (
+        <Slab my={2}>
+          <SlabBody>
+            <Text as={'span'}>Onboarding Link: </Text>{' '}
+            <Text
+              as={'a'}
+              href={onboardingUrl}
+              target="_blank"
+              color={'brand.500'}
+            >
+              {onboardingUrl}
+            </Text>
+          </SlabBody>
+        </Slab>
+      )}
       <Text>This is the dashboard</Text>
     </ManagerDashboardLayout>
   );
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,3 @@
+<?php
+$basePath = asset('domains/base') . '/';
+echo require_once public_path('domains/base/index.php');
