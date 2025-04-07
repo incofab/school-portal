124912b10d2a647af4a7dd6dd3cd8be15f973e7e
SirKris <kwadoblog@gmail.com>
2025-04-07 14:25:44 +0100

    07/04/2025 14:25

--- app/Http/Controllers/Institutions/Assignments/AssignmentController.php
+++ app/Http/Controllers/Institutions/Assignments/AssignmentController.php
@@ -40,18 +40,21 @@ class AssignmentController extends Controller
     $institutionUser = currentInstitutionUser();
 
     $query = Assignment::query()
-    ->init()
-    ->when(
-      $institutionUser->isStudent(),
-      fn(AssignmentQueryBuilder $q) => $q->forStudent($institutionUser->student()->firstOrFail())
-    )
-    ->when(
-      $institutionUser->isTeacher(),
-      fn($q) => $q->forTeacher($institutionUser)
-    )->with('course', 'classifications');
+      ->init()
+      ->when(
+        $institutionUser->isStudent(),
+        fn(AssignmentQueryBuilder $q) => $q->forStudent(
+          $institutionUser->student()->firstOrFail()
+        )
+      )
+      ->when(
+        $institutionUser->isTeacher(),
+        fn($q) => $q->forTeacher($institutionUser)
+      )
+      ->with('course', 'classifications');
 
     AssignmentUITableFilters::make($request->all(), $query)->filterQuery();
-    
+
     return Inertia::render('institutions/assignments/list-assignments', [
       'assignments' => paginateFromRequest($query->latest('assignments.id'))
     ]);
@@ -67,63 +70,20 @@ class AssignmentController extends Controller
     $assignment->load('classifications');
 
     return Inertia::render('institutions/assignments/create-edit-assignment', [
-      'assignment' => $assignment,
+      'assignment' => $assignment
     ]);
   }
 
   function show(Institution $institution, Assignment $assignment)
   {
-<<<<<<< HEAD
     $this->authorize('view', $assignment);
     $institutionUser = currentInstitutionUser();
-    
+
     abort_if(
       $institutionUser->isStudent() && now() > $assignment->expires_at,
       403,
       'Submission Deadline has passed.'
     );
-=======
-    $user = currentInstitutionUser();
-
-    if ($user->isStudent()) {
-      $currentTime = Carbon::now();
-      $student = currentInstitutionUser()
-        ->student()
-        ->with('classification')
-        ->first();
-      $submittedAssignments = AssignmentSubmission::where(
-        'student_id',
-        $student->id
-      )
-        ->with('classification')
-        ->pluck('assignment_id');
-
-      if (
-        $assignment->classification->classification_group_id !=
-        $student->classification->classification_group_id
-      ) {
-        abort(403, 'You are not eligible for this assignment.');
-      }
-
-      if ($currentTime > $assignment->expires_at) {
-        abort(403, 'Submission Deadline has passed.');
-      }
-
-      if ($submittedAssignments->contains($assignment->id)) {
-        abort(403, 'You have already submitted this assignment.');
-      }
-    } elseif ($user->isTeacher()) {
-      $course_teacher_user_id = $assignment->courseTeacher->user_id;
-      $current_user_id = $user->user->id;
-
-      if ($course_teacher_user_id != $current_user_id) {
-        abort(401, 'Unauthorized.');
-      }
-    } elseif ($user->isAdmin()) {
-    } else {
-      abort(401, 'Unauthorized');
-    }
->>>>>>> e42b5313d399e660cf1d35680def02e598a8171d
 
     return Inertia::render('institutions/assignments/show-assignment', [
       'assignment' => $assignment->load('course')
@@ -148,7 +108,11 @@ class AssignmentController extends Controller
   {
     $data = $request->validated();
 
-    (new RecordAssignment($institution, $request->getInstitutionUser(), $data))->create();
+    (new RecordAssignment(
+      $institution,
+      $request->getInstitutionUser(),
+      $data
+    ))->create();
 
     return $this->ok();
   }
@@ -160,9 +124,12 @@ class AssignmentController extends Controller
   ) {
     $data = $request->validated();
 
-    (new RecordAssignment($institution, $request->getInstitutionUser(), $data))->update($assignment);
-    
+    (new RecordAssignment(
+      $institution,
+      $request->getInstitutionUser(),
+      $data
+    ))->update($assignment);
+
     return $this->ok();
   }
-
 }
